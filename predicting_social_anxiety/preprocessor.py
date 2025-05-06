64439154ab44f9a5bbbce2689365085c12ed2c62
AsalSaud <asaalsaud06@gmail.com>
2025-05-06 11:45:17 +0300

    Add files via upload

--- predicting_social_anxiety/preprocessor.py
+++ predicting_social_anxiety/preprocessor.py
@@ -1,1 +1,17 @@
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
 
+
+def build_pipeline(cat_cols):
+
+    cat_transformer = Pipeline([
+        ('encoder' , OneHotEncoder(handle_unknown='ignore', sparse_output = False, drop='if_binary'))
+    ])
+
+    preprocessor = ColumnTransformer([
+        ('catagorial', cat_transformer, cat_cols)
+    ])
+
+    print('building pipeline done')
+    return preprocessor
